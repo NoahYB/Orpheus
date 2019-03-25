a30bca7d23af6aa91a5bff006da9aca207c70965
NoahYB <nybrinton@gmail.com>
2019-03-25 11:29:52 -0400

    added infinite platform generation

--- OrpheusGame/Assets/Scripts/PlatformGenerator.cs
+++ OrpheusGame/Assets/Scripts/PlatformGenerator.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour
 {
     public float moveSpeed = 0.1f;
-
-    public GameObject basicPlatform;
+    public float maxDistance;
+    public float distanceBetweenPlatforms;
+    public GameObject player;
+    public GameObject currPlatform;
+    public GameObject currPlatform2;
     public GameObject startingPlatform;
-
+    float y;
     // Start is called before the first frame update
     void Start()
     {
-        
+        y = currPlatform.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (startingPlatform != null)
+        if(currPlatform.transform.position.x < player.transform.position.x - maxDistance)
         {
-            //startingPlatform.transform.Translate(-0.1f, 0, 0);
+            Debug.Log("here");
+            float copyX = currPlatform2.transform.position.x;
+            currPlatform.transform.position = new Vector3(copyX+distanceBetweenPlatforms,y,0);
+        }
+        if (currPlatform2.transform.position.x < player.transform.position.x - maxDistance)
+        {
+            Debug.Log("here");
+            float copyX = currPlatform.transform.position.x;
+            currPlatform2.transform.position = new Vector3(copyX + distanceBetweenPlatforms, y, 0);
         }
     }
 }
