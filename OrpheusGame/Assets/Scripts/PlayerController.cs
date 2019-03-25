329799067a1e547bdfdf7e72a45f87b569d8a273
NoahYB <nybrinton@gmail.com>
2019-03-25 10:57:08 -0400

    added animations and jumping

--- OrpheusGame/Assets/Scripts/PlayerController.cs
+++ OrpheusGame/Assets/Scripts/PlayerController.cs
@@ -5,27 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Vector3 jump;
-    public float jumpForce = 2.0f;
-
-    [SerializeField]
+    Vector3 velocity;
+    public float jumpVelocity = 2.0f;
+    public float moveSpeed = 1.0f;
     Rigidbody2D rb;
-
+    public Collider2D playerCollider;
+    public LayerMask ground;
     public Animator animator;
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        playerCollider = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        animator.SetBool("isWalking", true);
     }
     public void Jump()
     {
-        Debug.Log("jumping");
-        rb.AddForce(jump * jumpForce, ForceMode2D.Impulse);
+        rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+
     }
     // Update is called once per frame
     void Update()
     {
-    
+
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics2D.IsTouchingLayers(playerCollider,ground))
+        {
+
+            Jump();
+        }
     }
 
 
