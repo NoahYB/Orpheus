d31bdec17238f256b90b1b417a083eedd0fb7012
Hector Meredith <hdm210@gmail.com>
2019-04-03 15:46:35 -0400

    added twitch client and api

---
+++ OrpheusGame/Assets/Scripts/TwitchAPI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TwitchLib.Unity;
+using TwitchLib.Api.Models.Undocumented.Chatters;
+
+public class TwitchAPI : MonoBehaviour
+{
+
+    public Api api;
+
+    [SerializeField]
+    TwitchClient tc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.runInBackground = true;
+        api = new Api();
+        api.Settings.AccessToken = Secrets.bot_access_token;
+        api.Settings.ClientId = Secrets.client_id;
+    }
+
+    private void GetChattersListCallback(List<ChatterFormatted> listOfChatters)
+    {
+        Debug.Log("List of " + listOfChatters.Count + "Viewers ");
+        foreach (var chatterObject in listOfChatters)
+        {
+            Debug.Log(chatterObject.Username);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            api.Invoke(api.Undocumented.GetChattersAsync(tc.client.JoinedChannels[0].Channel), GetChattersListCallback);
+        }
+    }
+}
